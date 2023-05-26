a54963ba02338677e7179465f1a95674d22c7023
Sandeep Dravid <sandeep.dravid@ansys.com>
2023-05-26 22:25:36 +0530

    added about page

---
+++ app/about/page.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='max-w-6x1 mx-auto space-y-4'> 
+      <h1 className='text-2x1 font-medium text-amber-600 space-x-4 '>About </h1>  
+      <p> 
+        Fit Social is SAAS platform for enablement of Sprots and Fitness community . 
+        It is an attempt to get like minded fitness lovers to come together to form groups , understand about the events happening in their areas , 
+        register for the events and brag about their achievements. 
+      </p>
+
+      <p>
+      It also is a place with mentors and trainers can showcase their skills to members . This platform will enable sports event managers to plan 
+      and execute events right from concept to social media marketing , manage registrations , event day planing , race day pictures and results. 
+      </p>
+
+    </div>
+  )
+}

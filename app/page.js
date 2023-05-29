6d3d066f0284f24f35678c71c071f693da689027
Sandeep Dravid <sandeep.dravid@ansys.com>
2023-05-29 15:39:26 +0530

    integrated with api

--- app/page.js
+++ app/page.js
@@ -1,9 +1,45 @@
 import Image from 'next/image'
+import Results from './components/Results'
 
-export default function Home() {
+const API_KEY = process.env.API_KEY ; 
+
+
+
+export default async function Home(searchParams) {
+  //const genre = searchParams.genre || "fetchTrending" 
+  const genre = searchParams.genre || "fetchTrending"
+    console.log(genre) ; 
+  //console.log( API_KEY )  ; 
+  const URL = `https://api.themoviedb.org/3/${
+    genre === "fetchTopRated" ? "movie/top_rated" : 
+    "trending/all/week"
+  }?api_key=${API_KEY}&Language=en-US&Page=1` ; 
+  console.log ( URL) ; 
+  
+  const res = await fetch(URL, {next : { revalidate : 10}});  
+
+  //const res = await fetch("https://api.themoviedb.org/3/trending/movie/week?language=en-US&page=1&api_key=d2c231a550db30d1d26c0f57b8ecf4b2");  
+
+  /*
+  ${
+    genre === "fetchTopRated" ? "movie/top_rated" : 
+    "trending/all/week?"
+  }api_key=${API_KEY}&Language=en-US&Page=1`,
+  { next : { revalidate : 10000}} 
+  ) ; 
+*/ 
+  if ( !res.ok) {
+    throw new Error ( " Failed to fetch data from server") ; 
+  }
+
+  const data = await res.json() ; 
+  const results = data.results ; 
+
+  //console.log( results) ; 
+
   return (
-    <div>
-     <h1 className='text-red-900'> HOME  </h1> 
+    <div> 
+       <Results results={results} />  
     </div>
     )
 }
